66723dd8687b8fac0576721c2df692d37bdd02ee
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-21 17:51:20 +0100

    feat: contraseña base de datos

--- bd_conecion.php
+++ bd_conecion.php
@@ -2,3 +2,4 @@
 // datos de la conexion a la base de datos
 $host="localhost"; // de momento como solo lo estamos probando lo dejaremos en localhost 
 $user="root"; //este es el usuario de la base de datos 
+$password"1234"; //contraseña de la base de datos
