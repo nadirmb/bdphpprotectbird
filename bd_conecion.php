92b25387d91b50677783dfd01e3ae1c1c1a6caca
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-21 17:48:07 +0100

    feat: probando_empezando php

---
+++ bd_conecion.php
@@ -0,0 +1,4 @@
+<?php
+// datos de la conexion a la base de datos
+$host="localhost"; // de momento como solo lo estamos probando lo dejaremos en localhost 
+$user="root"; //este es el usuario de la base de datos 
