69c436c0669367c5013f625d8c112ad02eee2a3e
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-25 20:13:51 +0100

    feat: conexion base de datos

--- bd_conecion.php
+++ bd_conecion.php
@@ -4,3 +4,6 @@ $host="localhost"; // de momento como solo lo estamos probando lo dejaremos en l
 $user="root"; //este es el usuario de la base de datos 
 $password"1234"; //contraseña de la base de datos
 $dbname = "protect_bird_db" // aqui ponemos el nombre de la base de datos
+
+// conexion con la base de datos
+$conn = new mysqli($host, $user, $password, $dbname);
