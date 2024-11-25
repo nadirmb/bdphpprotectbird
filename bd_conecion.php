31ec55bb330f366302303a50a63a28433babd9df
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-25 20:24:47 +0100

    feat: comprobar que la conexion es correcta

--- bd_conecion.php
+++ bd_conecion.php
@@ -1,9 +1,15 @@
 <?php
 // datos de la conexion a la base de datos
-$host="localhost"; // de momento como solo lo estamos probando lo dejaremos en localhost 
+$host="localhost"; //localhoostt
 $user="root"; //este es el usuario de la base de datos 
 $password"1234"; //contraseña de la base de datos
 $dbname = "protect_bird_db" // aqui ponemos el nombre de la base de datos
 
 // conexion con la base de datos
 $conn = new mysqli($host, $user, $password, $dbname);
+
+// comprobar que la conexion es correcta
+if ($conn→connect_error) {
+    die("Error de conexión: ". $conn→connect_error);
+}
+?>
