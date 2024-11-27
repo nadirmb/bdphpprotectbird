7c388adbc44632ede39fc3cbb9c13fb972378542
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-27 16:53:09 +0100

    feat: encriptar contraseña

--- crear_jugador.php
+++ crear_jugador.php
@@ -19,4 +19,4 @@ $stmt->execute();
 // para recibir datos del formulario
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
-$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT); // encriptar la contraseña, asin hay mas seguridad
+$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT); // encriptar la contraseña, asin hay mas seguridad en nuestra pagina
