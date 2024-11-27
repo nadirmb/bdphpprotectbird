17a0006860ffdd585c4f1bc646206bb565e8d170
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-27 16:00:01 +0100

    fix: vincular valor a parametro

--- crear_jugador.php
+++ crear_jugador.php
@@ -5,3 +5,7 @@ include 'bd_conecion.php';
 // hacer la consulta para insertar un nuevo jugador
 $sql = "INSERT INTO jugadores (nombre, correo, contraseña) VALUES (:nombre, :correo, :contraseña)";
 $stmt = $pdo->prepare($sql);
+
+// enlazar los parametros
+// Para enlazar los parametros utilizaremos bindparam para poder vincular un valor a un parametro en una sentencia
+// y asin los valores seran pasador de froma segura a la consulta
