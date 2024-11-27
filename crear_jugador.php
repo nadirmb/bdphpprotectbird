ea24331585e033554f2962f6aa82fedcab20e787
«Nadir» <nadirelmoussoui6@gmail.com>
2024-11-27 15:49:19 +0100

    feat:  consualta para insertar

--- crear_jugador.php
+++ crear_jugador.php
@@ -1,3 +1,7 @@
 <?php
 // Incluir archivo de configuración que hicimos anteriormente
 include 'bd_conecion.php';
+
+// hacer la consulta para insertar un nuevo jugador
+$sql = "INSERT INTO jugadores (nombre, correo, contraseña) VALUES (:nombre, :correo, :contraseña)";
+$stmt = $pdo->prepare($sql);
