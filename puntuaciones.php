ba1ea4525f5cdf1d9f8c602f0e912c79ca177b74
«Nadir» <nadirelmoussoui6@gmail.com>
2024-12-02 19:21:50 +0100

    feat: validar datos enviados

--- puntuaciones.php
+++ puntuaciones.php
@@ -1,3 +1,12 @@
 <?php
 //archivo de configuracion para la conexion a la bsae de datos
 require 'bd_conecion.php';
+
+//funcin para agregar una nueva puntuacion
+function agregarPuntuacion($conn) {
+//validar datos enviados del formulario/cliente
+if (isset($_POST['id_jugador']) && isset($_POST['puntuacion'])) {
+    $id_jugador = intval($_POST['id_jugador']);
+    $puntuacion = intval($_POST['puntuacion']);
+}
+}
