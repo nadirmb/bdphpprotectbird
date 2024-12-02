c692dfaf1f9ea8bffd5c2c4cd934686b8334ccc6
«Nadir» <nadirelmoussoui6@gmail.com>
2024-12-02 19:32:28 +0100

    feat: instertar puntuacion en la bd

--- puntuaciones.php
+++ puntuaciones.php
@@ -2,11 +2,15 @@
 //archivo de configuracion para la conexion a la bsae de datos
 require 'bd_conecion.php';
 
-//funcin para agregar una nueva puntuacion
+//funcion para agregar una nueva puntuacion
 function agregarPuntuacion($conn) {
 //validar datos enviados del formulario/cliente
 if (isset($_POST['id_jugador']) && isset($_POST['puntuacion'])) {
     $id_jugador = intval($_POST['id_jugador']);
     $puntuacion = intval($_POST['puntuacion']);
 }
+// Insertar la puntuación en la base de datos
+$sql = "INSERT INTO puntuaciones (jugador_id, puntuacion) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param($id_jugador, $puntuacion); // por seguridad utilizaremos bind_param 
 }
