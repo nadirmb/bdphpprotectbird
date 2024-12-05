9defb1290c7571111f21fd867f238e14954010d6
«Nadir» <nadirelmoussoui6@gmail.com>
2024-12-05 01:04:55 +0100

    fix: coorecion dbname,password

--- bd_conecion.php
+++ bd_conecion.php
@@ -2,8 +2,8 @@
 // datos de la conexion a la base de datos
 $host="localhost"; //localhoostt
 $user="root"; //este es el usuario de la base de datos 
-$password""; //contraseña de la base de datos
-$dbname = "protect_bird_db" // aqui ponemos el nombre de la base de datos
+$password=""; //contraseña de la base de datos
+$dbname = "protect_bird_db"; // aqui ponemos el nombre de la base de datos
 
 // conexion con la base de datos
 $conn = new mysqli($host, $user, $password, $dbname);
