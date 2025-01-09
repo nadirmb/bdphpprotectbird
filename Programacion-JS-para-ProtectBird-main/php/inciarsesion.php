094f49b8899d0c84294dacbe79c0baa4d934445b
«Nadir» <nadirelmoussoui6@gmail.com>
2025-01-09 16:49:27 +0100

    feat: conectar ala base de datos

--- Programacion-JS-para-ProtectBird-main/php/inciarsesion.php
+++ Programacion-JS-para-ProtectBird-main/php/inciarsesion.php
@@ -1,8 +1,20 @@
 <?php
 session_start(); 
 
-// verificar si el formulario fue enviado
+// comprobar si el formulario fue enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $pass = $_POST['password'];
+    
+    // conectar a la base de datos
+    $host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $dbname = "protect_bird_db";
+    $conn = new mysqli($host, $db_user, $db_pass, $dbname);
+    
+    // comprobar si la conexión ha sido correcta
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    
 }
