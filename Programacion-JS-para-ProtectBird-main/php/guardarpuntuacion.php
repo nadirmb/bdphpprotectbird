735210be8102764d4cd0d2e0d4ee7902767309fd
«Nadir» <nadirelmoussoui6@gmail.com>
2025-01-13 00:58:45 +0100

    feat:  verificar si es POST

--- Programacion-JS-para-ProtectBird-main/php/guardarpuntuacion.php
+++ Programacion-JS-para-ProtectBird-main/php/guardarpuntuacion.php
@@ -5,3 +5,8 @@ session_start();
 if (!isset($_SESSION['username'])) {
     exit("Debe iniciar sesión para guardar la puntuación."); // si no hay un usuario en la sesion se mostrara un mensaje y lo termina
 }
+
+// verificamos si la petición es POST
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    exit("El metodo no es permitido."); // si no es una peticion POST se muestra un mensaje de error y termina la ejecuiion
+}
