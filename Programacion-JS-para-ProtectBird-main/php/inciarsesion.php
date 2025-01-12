818243b11e65cf24060ffb42f6530a21ed07d26e
«Nadir» <nadirelmoussoui6@gmail.com>
2025-01-13 00:19:59 +0100

    fix: correcionde redirigir y comentarios

--- Programacion-JS-para-ProtectBird-main/php/inciarsesion.php
+++ Programacion-JS-para-ProtectBird-main/php/inciarsesion.php
@@ -11,30 +11,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db_user = "root";
     $db_pass = "";
     $dbname = "protect_bird_db";
+    
+    
+    // creamos la conexión con la base de datos
     $conn = new mysqli($host, $db_user, $db_pass, $dbname);
     
     // comprobar si la conexión ha sido correcta
     if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
+        die("Error de conexión: " . $conn->connect_error); // Termina el script si hay un error de conexión
     
 }
 
 // consultar si el nombre de usuario existe en la base de datos
   $stmt = $conn->prepare("SELECT * FROM jugadores WHERE nombre = ?");
-  $stmt->bind_param("s", $user); // 's' es para decir que es string
+  $stmt->bind_param("s", $user); // s es para decir que es string
   $stmt->execute();
-  $result = $stmt->get_result();
+  $result = $stmt->get_result(); // obtener los resultados de la consulta
 
-  if ($result->num_rows > 0) { // comprbamos si el usuario existe en la base de datos
-    $row = $result->fetch_assoc(); // obtnemos los datos del usuario desde la base de datos
-    if (password_verify($pass, $row['contraseña'])) { // comprobar si la contraseña que se ha puesto coincide con la guarada
+  if ($result->num_rows > 0) {  // comprobamos si el resultado de una consulta a la base de datos contiene al menos una fila es decir, si hay datos que coincidan con algunos de la consulta.
+    $row = $result->fetch_assoc(); // obtener los datos del usuario desde la base de datos
+
+    if (password_verify($pass, $row['contraseña'])) { // comprobamos si la contraseña que se ha puesto coincide con la guarada
         $_SESSION['username'] = $user; // guarda el nombre de usuario en la sesión para identificar al usuario
-        header("Location:https://localhost/Programacion-JS-para-ProtectBird-main/php/paginared.php"); // Redirige al usuario a la página paginared.php(es la pagina que se redigira) después de iniciar sesion
+        $_SESSION['id_jugador'] = $row['id']; // guradar el ID del jugador para usarlo más tarde
+
+        header("Location:https://localhost/Programacion-JS-para-ProtectBird-main/game/game.html"); // Redirigir al usuario al juego
     } else {
-        echo "Contraseña incorrecta."; 
+        echo "Contraseña incorrecta."; // si la contraseña es incorrecta, mostrar un mensaje de error
     }
 } else {
-    echo "El usuario no existe."; 
+    echo "El usuario no existe."; // si no se ecuentra al usuario en la base de datos, mostrara un mensaje de error
+
 }
 $stmt->close(); 
 $conn->close();
