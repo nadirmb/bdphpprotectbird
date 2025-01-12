04a72a82efeabdf2b959e4af76a0f7429ee861ef
«Nadir» <nadirelmoussoui6@gmail.com>
2025-01-13 00:48:58 +0100

    feat: verificamos inicio sesion

--- Programacion-JS-para-ProtectBird-main/php/guardarpuntuacion.php
+++ Programacion-JS-para-ProtectBird-main/php/guardarpuntuacion.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// comprobamos si el usuario ha iniciado sesion sino ha iniciado sesion que salga un mensaje
+// verificamos si el usuario ha iniciado sesion
 if (!isset($_SESSION['username'])) {
     exit("Debe iniciar sesión para guardar la puntuación.");
 }
