742894b4b4de6d99a9ea9296ec8ae10e7459c255
«Nadir» <nadirelmoussoui6@gmail.com>
2025-01-15 02:03:31 +0100

    feat: mensaje correo si se repite

--- Programacion-JS-para-ProtectBird-main/php/bd_conecion.php
+++ Programacion-JS-para-ProtectBird-main/php/bd_conecion.php
@@ -23,14 +23,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pass = $_POST['password']; // contraseña
     $confirm_pass = $_POST['confirm_password']; // esto sera la confirmacion de la contraseña
 
-// Verificar si las contraseñas coinciden
+// verifcar si las contraseñas coinciden
 if ($pass != $confirm_pass) {
         echo "Las contraseñas no coinciden. Por favor, intentalo de nuevo.";
         exit(); // Detener el script si las contraseñas no coinciden
     }
 
-// Encriptar la contraseña para mayor seguridad
+// encriptar la contraseña para mayor seguridad
     $pass_hashed = password_hash($pass, PASSWORD_DEFAULT);
+
+// revisamos si el correo ya esta registrado en la base de datos
+$check_email = $conn->prepare("SELECT id FROM jugadores WHERE correo = ?");
+
+// le decimos que use el correo que dio el usuario en la consulta
+$check_email->bind_param("s", $email);
+
+// ejecutamos la consulta para buscar en la base de datos
+$check_email->execute();
+
+// obtenemos el resultado para ver si la base de datos encontro algo
+$result = $check_email->get_result();
+
+// si se encuentra algun resultado significa que el correo ya existe
+if ($result->num_rows > 0) {
+    // mostramos un mensaje diciendo que el correo ya esta en uso
+    echo "El correo ya está registrado. Por favor, utiliza otro.";
+    // detenemos el codigo para que no siga intentando registrar
+    exit();
+}
 
 //consulta para insertar datos en la tabla jugadores
     $stmt = $conn->prepare("INSERT INTO jugadores (nombre, correo, contraseña) VALUES (?, ?, ?)");
